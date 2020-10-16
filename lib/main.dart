c9fe816b99ee84b1a2d278bcb5770a3042985c89
TinayMarydel <BG201801931@wmsu.edu.ph>
2020-10-16 12:16:33 +0800

    Hello World

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+//this function is the starting point for all our flutter apps.
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
